6d0359802176bca09771f28ae4ff8acd69920e91
N. Hatakeyama <52262200+naohata0902@users.noreply.github.com>
2025-06-21 13:30:19 +0000

    fix target_size calculation

--- pandas_search/pandas_search.py
+++ pandas_search/pandas_search.py
@@ -3,7 +3,7 @@ from collections.abc import Generator
 import pandas as pd
 import re
 
-from .area import Area
+from lib import Area, Cell
 
 class PandasSearch:
     """
@@ -33,52 +33,81 @@ class PandasSearch:
         else:
             return True
         
-    def search(self, regex: str,
+    def search(self, word: str,
                top_left_cell: tuple[int, int] = (0, 0),
-               bottom_right_cell: tuple[int, int] = (-1, -1)
+               bottom_right_cell: tuple[int, int] = (-1, -1),
+               exact_match: bool = False
                ) -> Generator[tuple[int, int], None, None]:
         """
         this gives the cell position matching to the regular expression
         in the area defined by top_left_cell and bottom_right_cell
         Parameter:
-            regex(str): key word which we want to find.
+            word(str): key word which we want to find.
                         It is defined by regular expression
             top_left_cell(tuple[int, int]):
                 top left cell position in any area within the target dataframe
             bottom_right_cell(tuple[int, int]):
                 bottom right cell position in any area within the target dataframe
+            exact_match(bool): default is Fault. this shows matching type
+                if you want to search word by exact match, this is set by True
         retrun:
             Generator: it shows cell position which is matched by regular expression
         """
+        if exact_match:
+            word = f"^{word}$"
         npdf = self.df.astype(str).to_numpy()
         area = Area(self.df, top_left_cell, bottom_right_cell)
 
-        for ir in range(area.top_left_cell.r, area.bottom_right_cell.r + 1):
-            for ic in range(area.top_left_cell.c, area.bottom_right_cell.c + 1):
+        for ir in range(area.top_left_cell.row, area.bottom_right_cell.row + 1):
+            for ic in range(area.top_left_cell.col, area.bottom_right_cell.col + 1):
                 cell_val = npdf[ir, ic]
-                if self.is_match(cell_val, regex):
+                if self.is_match(cell_val, word):
                     yield (ir, ic)
 
     def peek(self,
               searched_cells: Generator,
               shift: tuple[int, int] = (0, 0),
-              target_size: tuple[int, int] = (1,1)):
+              target_size: tuple[int, int] = (1,1)) -> list:
         """
+        this gives the data at the area which is based on the searched word
+        the area is far from discance which is defined by 'shift'
+        and size is defined by 'target_size'
+        Parameter:
+            shift(tuple[int, int]):
+                this shows a vector which means distance and direction
+                from the searched word 
+            target_size(typle[int, int]):
+                this shows the size of target area shown by row and column
+        Return:
+            (pd.DataFrame)
         """
+        target_dfs = []
         for cell in searched_cells:
-            i_r, i_c = cell
+            searched_cell = Cell(*cell)
 
-            target_cell = self.add_tuple(cell, shift)
-            target_cell = self.add_tuple(target_cell, target_size)
-            target_cell = self.add_tuple(target_cell, (-1, -1))
-            print(f"position : {cell}, val:{self.df.iloc[*target_cell]}")
+            top_left_cell = searched_cell + shift
+            bottom_right_cell = self.calc_bottom_right_cell(top_left_cell,
+                                                            target_size)
+            print(f"top_left_cell:{top_left_cell}, bottom_right_cell:{bottom_right_cell}")
 
-        return "answer"
-    
-    def add_tuple(self, cell_a: tuple, cell_b: tuple) -> tuple:
-        ar, ac = cell_a
-        br, bc = cell_b
-        
-        return (ar + br, ac + bc)
-    
-    
+            target_dfs.append(self.df.iloc[top_left_cell.row:bottom_right_cell.row,
+                                           top_left_cell.col:bottom_right_cell.col])
+
+
+        return target_dfs
+
+    def calc_bottom_right_cell(self, top_left_cell: Cell,
+                               size: tuple[int, int]) -> Cell:
+        """
+        """
+        size_row, size_col = size
+        if size_row < 0:
+            row = self.nr + size_row - top_left_cell.row + 1
+        else:
+            row = top_left_cell.row + size_row
+        if size_col < 0:
+            col = self.nc + size_col - top_left_cell.col + 1
+        else:
+            col = top_left_cell.col + size_col
+
+        return Cell(row, col)
