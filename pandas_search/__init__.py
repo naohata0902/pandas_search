e0cc593fc5c318e4d7b90f010e3e13210ed642f9
N. Hatakeyama <52262200+naohata0902@users.noreply.github.com>
2025-07-26 13:35:54 +0000

    update

--- pandas_search/__init__.py
+++
@@ -1,1 +0,0 @@
-from .pandas_search import PandasSearch
--- simple_run.py
+++ simple_run.py
@@ -1,25 +1,27 @@
 import pandas as pd
+from logging import getLogger
 from pandas_search.pandas_search import PandasSearch
+
+logger = getLogger()
 
 def test_run():
     df = pd.read_excel("./major_results_2020.xlsx")
     ps = PandasSearch(df)
 
-    print(ps.nr, ps.nc)
+    logger.info(f"{ps.nr=}:{ps.nc=}")
 
     ans = ps.is_match("abvccc", "^.bvc")
-    print("regex match: ", ans)
+    logger.info("regex match: ", ans)
 
     cells = ps.search("都道府県名", exact_match = True)
     """
     for cell in cells:
         print(f"position : {cell}, val:{df.iloc[*cell]}")
     """
-    print("test")
 
     dfs = ps.peek(cells, shift=(0, 0), target_size=(1, -1))
     for idf in dfs:
-        print(idf)
+        logger.info(idf)
  
 
 if __name__ == "__main__":
